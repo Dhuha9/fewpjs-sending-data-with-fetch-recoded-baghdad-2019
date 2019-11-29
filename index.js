1e280ff7e398f0fc1030df9a1d58670033b7bb6a
Dhuha Hasan <duha_9@yahoo.com>
2019-11-29 15:40:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, mail) {
+    let formData = {
+        userName: name,
+        email: mail
+    };
+    let configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configObject).then(res => res.json()).then(obj => {
+        let div = document.createElement("div")
+        div.innerHTML = obj.id
+        document.getElementsByTagName("body")[0].appendChild(div)
+    }).catch(error => {
+        let div = document.createElement("div")
+        div.innerHTML = error
+        document.getElementsByTagName("body")[0].appendChild(div)
+    })
+}
